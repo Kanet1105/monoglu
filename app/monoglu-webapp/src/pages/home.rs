7d2db19ca6c00eb60f43bc1cdb18dae9c090db65
Allen Clarence <zombiedelah@gmail.com>
2022-08-03 15:49:18 +0900

    Add the home page.

--- app/monoglu-webapp/src/pages/home.rs
+++ app/monoglu-webapp/src/pages/home.rs
@@ -1,29 +1,14 @@
+use crate::components::NavBar;
+use crate::components::SearchBar;
+
 use yew::prelude::*;
 
 #[function_component(Home)]
 pub fn home() -> Html {
     html! {
-        <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-            <a class="navbar-brand" href="/home">
-                // <img src= "" width="" height="" class="d-inline-block align-top" alt="">
-                { "MONOGLU" }
-            </a>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a></a>
-                    </li>
-                    <li class="nav-item">
-                    </li>
-                    <li class="nav-item">
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <>
+        <NavBar />
+        <SearchBar />
+        </>
     }
 }
