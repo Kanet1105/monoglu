b379431750b219cc8080d69f4627b7942034b67c
Allen Clarence <zombiedelah@gmail.com>
2022-08-09 12:49:10 +0900

    Set up a base template for the project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    println!("Hello, world!");
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Box::new(monoglu::TemplateApp::new(cc))),
+    );
 }
