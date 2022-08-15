65cffd9b0c99b94c2d0cdd785492ee4c16af93f4
Allen Clarence <zombiedelah@gmail.com>
2022-08-15 17:14:44 +0900

    Remove a redundant handle.

--- src/component/nav.rs
+++ src/component/nav.rs
@@ -12,21 +12,19 @@ impl Component for Navigator {
     fn view(&self, ctx: &egui::Context, event: crate::prelude::Event, state: crate::prelude::State) {
         egui::TopBottomPanel::top("nav").show(ctx, |ui| {
             ui.horizontal(|ui| {
-                let event_handle = event.clone();
-
                 let test = ui.button("Test");
                 if test.clicked() {
-                    event_handle.route(Self::Router::Test).unwrap();
+                    event.route(Self::Router::Test).unwrap();
                 };
 
                 let test1 = ui.button("Test1");
                 if test1.clicked() {
-                    event_handle.route(Self::Router::Test1).unwrap();
+                    event.route(Self::Router::Test1).unwrap();
                 };
 
                 let test2 = ui.button("Test2");
                 if test2.clicked() {
-                    event_handle.route(Self::Router::Test2).unwrap();
+                    event.route(Self::Router::Test2).unwrap();
                 };
             });
         });
