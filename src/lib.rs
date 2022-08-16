28250d5222edd5d327080aab8222896c507738dc
Allen Clarence <zombiedelah@gmail.com>
2022-08-16 09:21:45 +0900

    Add widget module in component/mod.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod prelude {
     pub use std::collections::{HashMap, VecDeque};
     pub use std::ops::Deref;
     pub use std::sync::{Arc, Mutex};
+    pub use std::time::{Duration, Instant};
 
     // app.rs
     pub use crate::app::Application;
@@ -15,6 +16,7 @@ pub mod prelude {
     // component module
     pub use crate::component::Component;
     pub use crate::component::page;
+    pub use crate::component::widget;
 
     // event.rs
     pub use crate::event::{Event, EventHandle};
