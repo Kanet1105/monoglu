5a76e5c0ba389d29a496370ac1b9dac012e1bba2
Allen Clarence <zombiedelah@gmail.com>
2022-08-20 16:49:59 +0900

    Top level components like ContextProvider<T> and BrowserRouter<T> goes into App component.

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+mod components;
+mod context_manager;
+mod pages;
+
+/// The module is used globally.
+pub mod prelude {
+    pub use std::cell::RefCell;
+    pub use std::collections::HashMap;
+    pub use std::fmt::Debug;
+    pub use std::ops::Deref;
+    pub use std::ptr;
+    pub use std::rc::Rc;
+
+    pub use crate::pages::test::Test;
+
+    pub use crate::components::prod::Producer;
+    pub use crate::components::sub::Subscriber;
+
+    pub use crate::context_manager::{ContextManager, Event, State};
+
+    pub use yew::prelude::*;
+    pub use yew::html::AnyScope;
+    pub use yew_router::prelude::*;
+}
+
+use crate::prelude::*;
+
+#[derive(Clone, PartialEq, Routable)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(route: &Route) -> Html {
+    match route {
+        Route::Home => html! { <Test /> },
+        Route::NotFound => html! { <h1>{ "404 : Not Found" }</h1> },
+    }
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    let manager = ContextManager::new();
+
+    html! {
+        <ContextProvider<ContextManager> context={manager.clone()}>
+            <BrowserRouter>
+                <Switch<Route> render={Switch::render(switch)} />
+            </BrowserRouter>
+        </ContextProvider<ContextManager>>
+    }
+}
