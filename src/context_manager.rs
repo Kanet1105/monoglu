dc9fb463c0b33fdd5739cd69f2ed68dab3fb555a
Allen Clarence <zombiedelah@gmail.com>
2022-08-21 14:52:21 +0900

    Refactor ContextManager API to hide the boilerplate.

--- src/context_manager.rs
+++ src/context_manager.rs
@@ -9,9 +9,12 @@ impl ContextManager {
         Self(Rc::new(RefCell::new(State::new())))
     }
 
-    pub fn insert(&self, id: &str, scope: AnyScope) -> Result<(), String> {
+    pub fn insert<T>(&self, id: &str, ctx: &Context<T>) -> Result<(), String> 
+    where
+        T: Component,
+    {
+        let scope = AnyScope::from(ctx.link().clone());
         let mut state = self.borrow_mut();
-
         match state.scope_map.contains_key(id) {
             true => Err(format!("Scope (id == {}) already exists..", id)),
             false => {
@@ -21,10 +24,18 @@ impl ContextManager {
         }
     }
 
-    pub fn get(&self, id: &str) -> Result<AnyScope, String> {
+    pub fn find<T>(&self, id: &str, component: T) -> Result<Scope<T>, String> 
+    where
+        T: Component,
+    {
         let state = self.borrow();
         match state.scope_map.get(id) {
-            Some(scope) => return Ok(scope.clone()),
+            // downscope does the casting operation from Anyscope to Scope<T> because
+            // Anyscope does not implement callback().
+            Some(anyscope) => {
+                let scope = anyscope.downcast::<T>();
+                Ok(scope)
+            },
             None => return Err(format!("Scope (id == {}) does not exist..", id)),
         }
     }
@@ -47,6 +58,22 @@ impl Deref for ContextManager {
 impl PartialEq for ContextManager {
     fn eq(&self, other: &Self) -> bool {
         Rc::ptr_eq(&self.0, &other.0)
+    }
+}
+
+/// Get a clone of ContextManager under a given context.
+/// It is used to call the current context manager 'T' provided by ContextProvider<T>.
+pub fn manager<T, C>(ctx: &Context<T>) -> Result<C, String>
+where
+    T: Component,
+    C: Clone + PartialEq + 'static,
+{
+    match ctx.link().context::<C>(Callback::noop()) {
+        Some(context) => {
+            let (manager, _) = context;
+            Ok(manager.clone())
+        },
+        None => Err(format!("No context found..")),
     }
 }
 
