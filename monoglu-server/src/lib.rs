e2d5fcbaa7cfe3597839311f66d3fc9de677630d
Allen Clarence <zombiedelah@gmail.com>
2022-08-27 01:10:21 +0900

    Add 'serde' and 'serde-json' dependencies.

--- monoglu-server/src/lib.rs
+++ monoglu-server/src/lib.rs
@@ -1,15 +1,47 @@
 mod auth;
 
 pub mod prelude {
+    pub use std::collections::BTreeMap;
     pub use std::path::PathBuf;
 
     pub use actix_files::{Files, NamedFile};
     pub use actix_web::{get, post, web};
     pub use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
     pub use actix_web::middleware::Logger;
+
+    pub use serde::{Serialize, Deserialize};
 }
 
 use crate::prelude::*;
+
+#[derive(Serialize)]
+pub struct NodeInfo {
+    pub user: String,
+    pub file_map: BTreeMap<String, Node>,
+}
+
+#[derive(Serialize)]
+pub struct Node {
+    pub node_type: NodeType,
+    pub name: String,
+    pub parent: Option<Node>,
+    pub children: Option<Vec<Node>>,
+    pub last_modified: String,
+    pub check_sum: String,
+}
+
+#[derive(Serialize)]
+pub enum NodeType {
+    Root,
+    Folder,
+    File,
+}
+
+#[get("/testget")]
+pub async fn test_get(request: HttpRequest) -> Result<impl Responder> {
+    
+    Ok(web::Json(object))
+}
 
 /// The entry point of the server.
 pub async fn run() -> std::io::Result<()> {
@@ -19,9 +51,10 @@ pub async fn run() -> std::io::Result<()> {
 
         App::new()
             .wrap(Logger::default())
+            .service(test_get)
             .service(webapp)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:60000")?
     .run()
     .await
 }
