be594593f4283fe43517620e5e582a8a4e67deb3
Allen Clarence <zombiedelah@gmail.com>
2022-08-29 03:17:49 +0900

    Add newtype for both 'local' and 'session' type storages.

--- monoglu-core/src/web/storage.rs
+++ monoglu-core/src/web/storage.rs
@@ -1,21 +1,13 @@
 use wasm_bindgen::prelude::*;
-use super::get_window;
-
-pub struct Storage(web_sys::Storage);
-
-impl Storage {
-    pub fn new() {
-        
-    }
-}
+use super::window;
 
 /// # Local Storage
 /// 
 /// Static local storage getter.
 /// 
 /// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-pub(crate) fn get_local_storage() -> web_sys::Storage {
-    get_window()
+pub(crate) fn local_storage() -> web_sys::Storage {
+    window()
         .local_storage()
         .expect_throw("Error unwrapping the local storage.")
         .expect_throw("Local Storage object is not available.")
@@ -26,9 +18,36 @@ pub(crate) fn get_local_storage() -> web_sys::Storage {
 /// Static session storage getter.
 /// 
 /// https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage
-pub(crate) fn get_session_storage() -> web_sys::Storage {
-    get_window()
+pub(crate) fn session_storage() -> web_sys::Storage {
+    window()
         .session_storage()
         .expect_throw("Error unwrapping the local storage.")
         .expect_throw("Session Storage object is not available.")
+}
+
+#[wasm_bindgen]
+pub struct Storage(web_sys::Storage);
+
+#[wasm_bindgen]
+pub enum StorageType {
+    Local,
+    Session,
+}
+
+#[wasm_bindgen]
+impl Storage {
+    pub fn new(storage_type: StorageType) -> Self {
+        match storage_type {
+            StorageType::Local => {
+                let local_storage = local_storage();
+
+                Self(local_storage)
+            },
+            StorageType::Session => {
+                let session_storage = session_storage();
+
+                Self(session_storage)
+            }
+        }
+    }
 }
