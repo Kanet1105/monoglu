b52dedd75df41ce98cf3ffd2a0dad99d4b940b29
Allen Clarence <zombiedelah@gmail.com>
2022-08-29 03:16:41 +0900

    Provide wrappers to web-sys low level bindings.

--- monoglu-core/src/web/mod.rs
+++ monoglu-core/src/web/mod.rs
@@ -1,5 +1,23 @@
 mod performance;
 mod storage;
+mod task;
 mod window;
 
-pub(crate) use window::get_window;
+pub(crate) use window::window;
+pub use storage::{Storage, StorageType}; 
+
+pub struct WebAPI {
+    pub window: web_sys::Window,
+    pub local_storage: Storage,
+    pub session_storage: Storage,
+}
+
+impl WebAPI {
+    pub fn new() -> Self {
+        Self {
+            window: window(),
+            local_storage: Storage::new(StorageType::Local),
+            session_storage: Storage::new(StorageType::Session),
+        }
+    }
+}
