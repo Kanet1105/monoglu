0f03849881678fa5b4702409b1614a62c67f7456
Allen Clarence <zombiedelah@gmail.com>
2022-08-30 13:50:40 +0900

    Intermediate commit..

--- monoglu-core/src/lib.rs
+++ monoglu-core/src/lib.rs
@@ -6,11 +6,11 @@ pub mod prelude {
 }
 
 use std::sync::{Arc, Once};
-
 use crate::web::WebAPI;
-use crate::web::{Method, Mode};
 
+use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::{spawn_local, JsFuture};
 use wasm_logger;
 use log::Level;
 
@@ -42,8 +42,8 @@ use log::Level;
 pub(crate) static mut RESOURCE_HANDLE: Option<*const WebAPI> = None;
 static INIT: Once = Once::new();
 
-/// ['crate::init_once()'] provides one-time global initiation constructed with
-/// ['std::sync::Once::new()']. 'Once' can be in one of the following
+/// ['crate::init_once()'] provides one-time global initiation constructed 
+/// with ['std::sync::Once::new()']. 'Once' can be in one of the following
 /// four states.
 /// 
 /// - const INCOMPLETE: usize = 0x0;
@@ -82,8 +82,19 @@ pub fn init_once() -> Result<(), String> {
     }
 }
 
-/// ['crate::web_api()'] is a public interface from which the user
-/// can get a thread-safe shared pointer Arc<T> to the raw data.
+/// [monoglu_core::web_api()] is a public interface from which the user
+/// can get a thread-safe shared pointer Arc<web_sys::Window>.
+/// 
+/// It is important to note that [web_sys::window()] also provides
+/// a safely sharable Javascript Window object, but it is not really
+/// convenient to use in the rust code because most of the API call
+/// returns [Result<web_sys::T, JsValue>], [Option<T>] or even
+/// [Result<Option<T>, JsValue>]. 
+/// 
+/// To minimize the cumbersome nested error handling and mild overhead
+/// of re-creating a javascript window object handle. ['web_sys::Window']
+/// is made static and wrapped in [std::sync::Arc] to be easily shareable 
+/// inside the rust code. 
 pub fn web_api() -> Result<Arc<WebAPI>, ()> {
     if !INIT.is_completed() {
         wasm_logger::init(wasm_logger::Config::new(Level::Info));
@@ -94,8 +105,6 @@ pub fn web_api() -> Result<Arc<WebAPI>, ()> {
             match RESOURCE_HANDLE {
                 Some(raw_ptr) => {
                     let web_api = Arc::from_raw(raw_ptr);
-                    log::info!("{:?}", Arc::as_ptr(&web_api));
-                    
                     Ok(web_api)
                 },
                 None => {
@@ -126,11 +135,28 @@ pub fn web_api() -> Result<Arc<WebAPI>, ()> {
 //     Ok(())
 // }
 
-// #[wasm_bindgen(start)]
-// pub async fn start_app() -> Result<(), JsValue> {
-//     init_once().unwrap();
-//     let api = web_api().unwrap();
-//     let response = api.fetch("https://google.com", Method::Get, Mode::CORS);
-//     api.task_spawn(response);
-//     Ok(())
-// }
+#[wasm_bindgen(start)]
+pub async fn start_app() -> Result<(), JsValue> {
+    init_once().unwrap();
+    let api = web_api().unwrap();
+    let a = api.average_performance(test1, 10);
+    let b = api.average_performance(test2, 10);
+    log::info!("{}", a - b);
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn test1() {
+    let window = web_sys::window().unwrap();
+    let storage = window.local_storage()
+        .unwrap()
+        .unwrap();
+    storage.set_item("key", "123").unwrap();
+}
+
+#[wasm_bindgen]
+pub fn test2() {
+    let api = web_api().unwrap();
+    let storage = api.local_storage();
+    storage.set_item("key", "123");
+}
