0da4bbb550493c4b806c4400d373210f884a9243
Allen Clarence <zombiedelah@gmail.com>
2022-09-15 13:26:02 +0900

    Add modules.

--- monoglu-core/src/lib.rs
+++ monoglu-core/src/lib.rs
@@ -1,34 +1,50 @@
+mod auth;
+mod config;
+mod error;
+mod state;
 mod storage;
 
+use crate::error::Exception;
 use axum::{
-    body::Bytes,
-    error_handling::HandleErrorLayer,
+    // body::Bytes,
+    // error_handling::HandleErrorLayer,
     handler::Handler,
-    http::StatusCode,
-    response::{IntoResponse, Response},
+    // http::StatusCode,
+    // response::{IntoResponse, Response},
     routing::{get, post},
     Router,
 };
-use std::net::SocketAddr;
-use serde::{Deserialize, Serialize};
+use state::SharedState;
+// use serde::{Deserialize, Serialize};
+use std::{env, net::SocketAddr};
+use tracing::{error, info};
+use tracing_subscriber;
 
 pub async fn run_app() {
-    let subscriber = tracing_subscriber::fmt()
+    // env::set_var("RUST_BACKTRACE", "full");
+
+    // Initialize the global tracing subscriber.
+    tracing_subscriber::fmt()
         .with_line_number(true)
         .with_thread_ids(true)
-        .finish();
+        .init();
 
-    let app = Router::new()
-        .route("/:file_name", get(get_file));
+    let shared_state = SharedState::new().map_err(|error| {
+        error!(error);
+        panic!();
+    });
+
+    // let database = Storage::new().await.unwrap();
+    // info!("Connected to the database.");
+    let app = Router::new().route("/", get(google_signin));
+    info!("Initialize the server.");
 
     let address = SocketAddr::from(([127, 0, 0, 1], 8080));
-    tracing::debug!("Listening on {}..", address);
+    info!("Listening on {}..", address);
     axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-pub async fn get_file(file_name: String) -> String {
-    file_name
-}
+pub async fn google_signin() {}
