a98b94230782ac1c9312ae9b58c08c5903f2892d
Allen Clarence <zombiedelah@gmail.com>
2022-09-19 19:11:32 +0900

    a storage module built on top of 'object_store' crate.

--- monoglu-storage/src/main.rs
+++ monoglu-storage/src/main.rs
@@ -1,25 +1,30 @@
+use bytes::Bytes;
+use object_store::{local::LocalFileSystem, path::Path, ObjectStore};
 use std::{
     env::current_dir,
     fs::File,
-    io::{BufReader, Read},
+    io::{BufRead, BufReader, Read},
     os::windows::fs::MetadataExt,
     path::PathBuf,
+    sync::Arc,
 };
 
 pub type Exception = Box<dyn std::error::Error>;
 
 #[tokio::main]
 async fn main() -> Result<(), Exception> {
-    let storage_path = path_from_vec(
-        vec!["D:/", "RustProjects", "monoglu", "monoglu-storage", "data"]
-    )?;
-    let tree = sled::open(storage_path)?;
+    let storage_path = path_from_vec(vec![
+        "D:/",
+        "RustProjects",
+        "monoglu",
+        "monoglu-storage",
+        "data",
+    ])?;
+    let local = LocalFileSystem::new_with_prefix(&storage_path.as_path())?;
+    let storage: Arc<dyn ObjectStore> = Arc::new(local);
 
-    let file_path = path_from_vec(
-        // vec!["D:/", "downloads", "All_Amazon_Review.json.gz"]
-        vec!["D:/", "downloads", "mongosh-1.5.4-x64.msi"]
-    )?;
-    read_file_bytes(file_path)?;
+    let file_path = path_from_vec(vec!["D:/", "downloads", "mongosh-1.5.4-x64.msi"])?;
+    upload(&file_path, storage.clone(), &storage_path).await?;
     Ok(())
 }
 
@@ -31,38 +36,70 @@ fn path_from_vec(v: Vec<&str>) -> Result<PathBuf, Exception> {
     Ok(path)
 }
 
-fn read_file_bytes(file_path: PathBuf) -> Result<(), Exception> {
+async fn upload(
+    file_path: &PathBuf,
+    storage: Arc<dyn ObjectStore>,
+    storage_path: &PathBuf,
+) -> Result<(), Exception> {
     let file = File::open(file_path)?;
     let metadata = file.metadata()?;
     let file_size = metadata.file_size();
-    
+
     let index_len: u64 = file_size / (1024 * 255);
     dbg!(index_len + 1);
     let mut buf_reader = BufReader::with_capacity(1024 * 255, file);
 
-    for index in 0..(index_len + 1) as usize {
-        dbg!(index);
-        let mut buffer = [0; 1024 * 255];
+    for suffix in 0..(index_len + 1) as usize {
+        let file_name = Path::from(suffix.to_string());
+        dbg!(&file_name);
+
+        let mut buffer = Vec::with_capacity(1024 * 255);
         buf_reader.read(&mut buffer)?;
+        let bytes = Bytes::from(buffer);
+        storage.put(&file_name, bytes).await?;
     }
     Ok(())
 }
 
-fn write_bytes(storage_path: PathBuf) {
-    
-}
+// // Sled usage.
+// fn sled_usage() -> Result<(), Exception> {
+//     use sled::{Db, IVec};
 
-// pub struct Object {
-//     capacity: usize,
+//     let storage_path = path_from_vec(
+//         vec!["D:/", "RustProjects", "monoglu", "monoglu-storage", "data"]
+//     )?;
+//     let db = sled::open(storage_path)?;
+
+//     let file_path = path_from_vec(
+//         // vec!["D:/", "downloads", "All_Amazon_Review.json.gz"]
+//         vec!["D:/", "downloads", "mongosh-1.5.4-x64.msi"]
+//     )?;
+//     upload_file_bytes(file_path, db.clone())?;
+//     Ok(())
 // }
 
-// impl Object {
-//     fn new() -> Self {
-//         Self { capacity: 255 * 1024 }
+// fn upload_file_bytes(file_path: PathBuf, db: Db) -> Result<(), Exception> {
+//     let file = File::open(&file_path)?;
+//     let metadata = file.metadata()?;
+//     let file_size = metadata.file_size();
+
+//     let index_len: u64 = file_size / (1024 * 255);
+//     dbg!(index_len + 1);
+//     let mut buf_reader = BufReader::with_capacity(1024 * 255, file);
+
+//     for index in 0..(index_len + 1) as usize {
+//         let key = format!("{}-{}", &file_path.to_str().unwrap(), index);
+//         dbg!(&key);
+//         let mut buffer = Vec::with_capacity(1024 * 255);
+//         buf_reader.read(&mut buffer)?;
+
+//         db.insert(IVec::from(key.as_bytes()), IVec::from(buffer))?;
 //     }
 
-//     fn read<T: Read>(&self, reader: &mut T) {
-//         let mut buffer = Vec::with_capacity(255 * 1024);
-//         let part = reader.take(self.capacity as u64);
+//     for index in 0..(index_len + 1) as usize {
+//         let key = format!("{}-{}", &file_path.to_str().unwrap(), index);
+//         let bytes = db.get(key.as_bytes())?.unwrap();
+//         dbg!(bytes);
 //     }
+//     Ok(())
 // }
