9c8997bc63436e53c78010cf1d1221dc68f7d34f
Allen Clarence <zombiedelah@gmail.com>
2022-09-20 20:03:32 +0900

    Remove sled examples.

--- monoglu-storage/src/main.rs
+++ monoglu-storage/src/main.rs
@@ -3,7 +3,7 @@ use object_store::{local::LocalFileSystem, path::Path, ObjectStore};
 use std::{
     env::current_dir,
     fs::File,
-    io::{BufRead, BufReader, Read},
+    io::{BufReader, Read},
     os::windows::fs::MetadataExt,
     path::PathBuf,
     sync::Arc,
@@ -24,7 +24,7 @@ async fn main() -> Result<(), Exception> {
     let storage: Arc<dyn ObjectStore> = Arc::new(local);
 
     let file_path = path_from_vec(vec!["D:/", "downloads", "mongosh-1.5.4-x64.msi"])?;
-    upload(&file_path, storage.clone(), &storage_path).await?;
+    upload(&file_path, storage.clone()).await?;
     Ok(())
 }
 
@@ -39,7 +39,6 @@ fn path_from_vec(v: Vec<&str>) -> Result<PathBuf, Exception> {
 async fn upload(
     file_path: &PathBuf,
     storage: Arc<dyn ObjectStore>,
-    storage_path: &PathBuf,
 ) -> Result<(), Exception> {
     let file = File::open(file_path)?;
     let metadata = file.metadata()?;
@@ -60,46 +59,3 @@ async fn upload(
     }
     Ok(())
 }
-
-// // Sled usage.
-// fn sled_usage() -> Result<(), Exception> {
-//     use sled::{Db, IVec};
-
-//     let storage_path = path_from_vec(
-//         vec!["D:/", "RustProjects", "monoglu", "monoglu-storage", "data"]
-//     )?;
-//     let db = sled::open(storage_path)?;
-
-//     let file_path = path_from_vec(
-//         // vec!["D:/", "downloads", "All_Amazon_Review.json.gz"]
-//         vec!["D:/", "downloads", "mongosh-1.5.4-x64.msi"]
-//     )?;
-//     upload_file_bytes(file_path, db.clone())?;
-//     Ok(())
-// }
-
-// fn upload_file_bytes(file_path: PathBuf, db: Db) -> Result<(), Exception> {
-//     let file = File::open(&file_path)?;
-//     let metadata = file.metadata()?;
-//     let file_size = metadata.file_size();
-
-//     let index_len: u64 = file_size / (1024 * 255);
-//     dbg!(index_len + 1);
-//     let mut buf_reader = BufReader::with_capacity(1024 * 255, file);
-
-//     for index in 0..(index_len + 1) as usize {
-//         let key = format!("{}-{}", &file_path.to_str().unwrap(), index);
-//         dbg!(&key);
-//         let mut buffer = Vec::with_capacity(1024 * 255);
-//         buf_reader.read(&mut buffer)?;
-
-//         db.insert(IVec::from(key.as_bytes()), IVec::from(buffer))?;
-//     }
-
-//     for index in 0..(index_len + 1) as usize {
-//         let key = format!("{}-{}", &file_path.to_str().unwrap(), index);
-//         let bytes = db.get(key.as_bytes())?.unwrap();
-//         dbg!(bytes);
-//     }
-//     Ok(())
-// }
