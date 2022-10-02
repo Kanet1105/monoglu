e865c54f8b085841e704aec49a2483cadf2bfd2c
Allen Clarence <zombiedelah@gmail.com>
2022-10-02 12:27:34 +0900

    Remove the worker and the storage crate.

--- monoglu-worker/src/main.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Error;
-use monoglu_worker::run_app;
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt::init();
-    run_app().await?;
-    Ok(())
-}
