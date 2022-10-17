b84410554edae5b25c9606f0ef093a5f4b5809f9
Allen Clarence <zombiedelah@gmail.com>
2022-10-17 18:04:38 +0900

    Add components.

--- monoglu-webapp/src/lib.rs
+++ monoglu-webapp/src/lib.rs
@@ -1,5 +1,41 @@
 mod apps;
 
+struct WebApp;
+
+impl WebApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self
+    }
+
+    fn navigation_bar(&self, ctx: &egui::Context, ratio: f32) {
+        egui::TopBottomPanel::top("navigation_bar")
+            .min_height(ctx.available_rect().height() * ratio)
+            .resizable(false)
+            .show(ctx, |ui| {});
+    }
+
+    fn side_bar(&self, ctx: &egui::Context, ratio: f32) {
+        egui::SidePanel::left("side_bar")
+            .min_width(ctx.available_rect().width() * ratio)
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.heading("Apps");
+                ui.separator();
+            });
+    }
+
+    fn ui(&self, ctx: &egui::Context) {
+        self.navigation_bar(ctx, 0.05);
+        self.side_bar(ctx, 0.1);
+    }
+}
+
+impl eframe::App for WebApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.ui(ctx);
+    }
+}
+
 #[cfg(target_arch = "wasm32")]
 pub fn run() {
     // Make sure panics are logged using `console.error`.
@@ -15,7 +51,7 @@ pub fn run() {
     eframe::start_web(
         "main_canvas", // hardcode it
         web_options,
-        Box::new(|cc| Box::new(apps::WebApp::new(cc))),
+        Box::new(|cc| Box::new(WebApp::new(cc))),
     )
     .expect("failed to start eframe");
 }
