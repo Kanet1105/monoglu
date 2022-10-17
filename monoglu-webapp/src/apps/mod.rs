2c8e33fe3bb3af4397d3f402d00d055e4236e43a
Allen Clarence <zombiedelah@gmail.com>
2022-10-17 18:05:15 +0900

    Move WebApp struct to lib.rs.

--- monoglu-webapp/src/apps/mod.rs
+++ monoglu-webapp/src/apps/mod.rs
@@ -6,17 +6,3 @@ pub trait View {
 }
 
 pub struct AppState {}
-
-pub struct WebApp;
-
-impl WebApp {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        Self
-    }
-}
-
-impl eframe::App for WebApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        
-    }
-}
