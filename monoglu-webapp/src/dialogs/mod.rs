8a43ffa3012ffd65a1e3b7eb94cc101f9419d5c4
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-10-19 15:13:58 +0900

    Add basic dialog features.

--- monoglu-webapp/src/dialogs/mod.rs
+++ monoglu-webapp/src/dialogs/mod.rs
@@ -1,3 +1,31 @@
 pub trait Dialog {
+    fn name(&self) -> &'static str;
+    fn is_visible(&self) -> bool;
     fn show(&self, ctx: &egui::Context);
+}
+
+pub struct DialogStates {
+    dialog_list: Vec<Box<dyn Dialog>>,
+}
+
+impl DialogStates {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        for dialog in &self.dialog_list {
+            if dialog.is_visible() {
+                dialog.show(ctx);
+            }
+        }
+    }
+}
+
+impl Default for DialogStates {
+    fn default() -> Self {
+        Self {
+            dialog_list: vec![],
+        }
+    }
 }
