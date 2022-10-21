754058e6126f9c79d804b02a650f2370fc5ca738
y2gon2 <y2gon2@gmail.com>
2022-10-21 16:36:05 +0900

    temp: use custom widget - button

--- monoglu-webapp/src/dialogs/mod.rs
+++ monoglu-webapp/src/dialogs/mod.rs
@@ -1,3 +1,5 @@
+use egui::NumExt;
+
 mod chat;
 mod graph1;
 
@@ -22,7 +24,10 @@ impl DialogStates {
                 ui.separator();
 
                 for dialog in &mut self.dialogs {
-                    let button = ui.button(dialog.name());
+                    let button = ui.add(
+                        SideButton::new(dialog.name())
+                    
+                    );
                     if button.clicked() {
                         dialog.toggle_visible();
                     }
@@ -42,5 +47,103 @@ impl Default for DialogStates {
                 Box::new(graph1::Graph1::new()),
             ],
         }
+    }
+}
+
+
+pub struct SideButton {
+    text: egui::WidgetText,
+    wrap: Option<bool>,
+    fill: Option<egui::Color32>,
+    stroke: Option<egui::Stroke>,
+    sense: egui::Sense,
+    small: bool,
+    frame: Option<bool>,
+    min_size: egui::Vec2,
+    image: Option<egui::widgets::Image>,
+}
+
+impl SideButton {
+    pub fn new(text: impl Into<egui::WidgetText>) -> Self {
+        Self {
+            text: text.into(),
+            wrap: None,
+            fill: Some(egui::Color32::WHITE),
+            stroke: Some(egui::Stroke { width: 1.0, color: egui::Color32::BLUE }),
+            sense: egui::Sense::click(),
+            small: false,
+            frame: None,
+            min_size: egui::Vec2::ZERO,
+            image: None,
+        }
+    }
+}
+
+impl egui::Widget for SideButton {
+    fn ui(self, ui: &mut egui::Ui) -> egui::Response {
+        let SideButton {
+            text,
+            wrap,
+            fill,
+            stroke,
+            sense,
+            small,
+            frame,
+            min_size,
+            image,
+        } = self;
+
+        let frame = frame.unwrap_or_else(|| ui.visuals().button_frame);
+
+        let mut button_padding = ui.spacing().button_padding + egui::vec2(30.0, 30.0);
+        
+        let total_extra = button_padding + button_padding;
+
+        let wrap_width = ui.available_width() - total_extra.x;
+        let text = text.into_galley(ui, wrap, wrap_width, egui::TextStyle::Button);
+
+        let mut desired_size = text.size() + 2.0 * button_padding;
+        desired_size = desired_size.at_least(min_size);
+
+        let (rect, response) = ui.allocate_at_least(desired_size, sense);
+        response.widget_info(|| egui::WidgetInfo::labeled(egui::WidgetType::Button, text.text()));
+
+        if ui.is_rect_visible(rect) {
+            let visuals = ui.style().interact(&response);
+            let text_pos = if let Some(image) = image {
+                let icon_spacing = ui.spacing().icon_spacing;
+                egui::emath::pos2 (
+                    rect.min.x + button_padding.x + image.size().x + icon_spacing,
+                    rect.center().y - 0.5 * text.size().y,
+                )
+            } else {
+                ui.layout()
+                    .align_size_within_rect(text.size(), rect.shrink2(button_padding))
+                    .min
+            };
+
+            if frame {
+                let fill = fill.unwrap_or(visuals.bg_fill);
+                let stroke = stroke.unwrap_or(visuals.bg_stroke);
+                ui.painter().rect(
+                    rect.expand(visuals.expansion),
+                    visuals.rounding,
+                    fill,
+                    stroke,
+                );
+            }
+
+            text.paint_with_visuals(ui.painter(), text_pos, visuals);
+        }
+
+        if let Some(image) = image {
+            let image_rect = egui::Rect::from_min_size(
+                egui::pos2(rect.min.x, rect.center().y -0.5 - (image.size().y / 2.0)),
+                image.size(),
+            );
+            image.paint_at(ui, image_rect);
+        }
+
+        response
     }
 }
