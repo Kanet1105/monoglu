6d29e8e735829a35668d0dffc5c2a9de30aa4b82
Allen Clarence <zombiedelah@gmail.com>
2022-10-23 23:18:44 +0900

    grid ex..

--- monoglu-webapp/src/tabs/user.rs
+++ monoglu-webapp/src/tabs/user.rs
@@ -13,7 +13,46 @@ impl super::Tab for User {
 
     fn view(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(self.name());
+            let frame_1 = egui::Frame::none()
+                .outer_margin(egui::style::Margin {
+                    left: 0.0,
+                    right: 0.0,
+                    top: 0.0,
+                    bottom: 0.0,
+                })
+                .inner_margin(egui::style::Margin::same(100.0))
+                .fill(egui::Color32::BLACK)
+                .show(ui, |ui| {
+                    ui.label("frame_1");
+                });
+
+            ui.horizontal(|ui| {
+                let frame_2 = egui::Frame::none()
+                    .outer_margin(egui::style::Margin {
+                        left: 0.0,
+                        right: 0.0,
+                        top: 0.0,
+                        bottom: 0.0,
+                    })
+                    .inner_margin(egui::style::Margin::same(100.0))
+                    .fill(egui::Color32::GREEN)
+                    .show(ui, |ui| {
+                        ui.label("frame_2");
+                    });
+
+                let frame_3 = egui::Frame::none()
+                    .outer_margin(egui::style::Margin {
+                        left: 0.0,
+                        right: 0.0,
+                        top: 0.0,
+                        bottom: 0.0,
+                    })
+                    .inner_margin(egui::style::Margin::same(100.0))
+                    .fill(egui::Color32::GRAY)
+                    .show(ui, |ui| {
+                        ui.label("frame_3");
+                    });
+            });
         });
     }
 }
