179b17d634516614cea9bf798cc38f856bcbf71f
Allen Clarence <zombiedelah@gmail.com>
2022-10-25 02:11:04 +0900

    Grid and Cell layout. Work in progress..

--- monoglu-webapp/src/tabs/user.rs
+++ monoglu-webapp/src/tabs/user.rs
@@ -1,10 +1,12 @@
-use crate::cell::Cell;
+use crate::cell::{Grid, Cell};
 
-pub struct User;
+pub struct User {
+    grid: Grid,
+}
 
 impl User {
     pub fn new() -> Self {
-        Self
+        Self { grid: Grid::new("user_tab_grid", 2, 2) }
     }
 }
 
@@ -15,13 +17,7 @@ impl super::Tab for User {
 
     fn view(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let mut cell = Cell::new("cell_grid", 2, 2);
-            let area_0_0 = cell
-                .get_area(0, 0);
-            // cell.get_frame(0, 0).fill = egui::Color32::BLUE;
-            // cell.get_frame(0, 1).fill = egui::Color32::RED;
-            // cell.get_frame(1, 0).fill = egui::Color32::GREEN;
-            // cell.get_frame(1, 1).fill = egui::Color32::BLACK;
+            self.grid.show(ctx);
         });
     }
 }
