d3e0d1bb5e00d2acd4b7bb9b53f2da324be6a3b3
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-11-16 14:35:26 +0900

    run() => run_server().

--- monoglu-platter/src/bin/server.rs
+++ monoglu-platter/src/bin/server.rs
@@ -1,6 +1,12 @@
 use monoglu_platter::prelude::*;
+use tracing_subscriber::fmt::time;
 
 #[tokio::main]
-async fn main() {
-    run().await.unwrap();
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_timer(time::LocalTime::rfc_3339())
+        .init();
+    run_server().await?;
+    info!("Shutting down..");
+    Ok(())
 }
--- monoglu-platter/src/lib.rs
+++ monoglu-platter/src/lib.rs
@@ -4,7 +4,7 @@ pub mod prelude {
     pub type Exception = Box<dyn std::error::Error>;
     pub type Result<T> = std::result::Result<T, Exception>;
 
-    pub use crate::run;
+    pub use crate::run_server;
     pub use tracing::{error, info};
 }
 
@@ -14,7 +14,7 @@ use tokio::{
     io::AsyncWriteExt
 };
 
-pub async fn run() -> Result<()> {
+pub async fn run_server() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7627").await?;
     
     loop {
