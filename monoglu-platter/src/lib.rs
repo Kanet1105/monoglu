3bd5fc5c5356dc077e27368c2250fd9375641fff
Allen Clarence <zombiedelah@gmail.com>
2022-11-16 11:34:10 +0900

    work in progress..

---
+++ monoglu-platter/src/lib.rs
@@ -0,0 +1,30 @@
+mod configuration;
+
+pub mod prelude {
+    pub type Exception = Box<dyn std::error::Error>;
+    pub type Result<T> = std::result::Result<T, Exception>;
+
+    pub use crate::run;
+    pub use tracing::{error, info};
+}
+
+use crate::prelude::*;
+use tokio::{
+    net::{TcpListener, TcpStream}, 
+    io::AsyncWriteExt
+};
+
+pub async fn run() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7627").await?;
+    
+    loop {
+        let (stream, _address) = listener.accept().await?;
+        connection_handler(stream).await?;
+    }
+}
+
+pub async fn connection_handler(mut stream: TcpStream) -> Result<()> {
+    
+    stream.shutdown().await?;
+    Ok(())
+}
