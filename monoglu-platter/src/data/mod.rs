353f89bfa9d48627e36a5720f4d17f5fe07cad1f
Allen Clarence <zombiedelah@gmail.com>
2022-11-17 22:45:22 +0900

    Add crates.

--- monoglu-platter/src/data/mod.rs
+++ monoglu-platter/src/data/mod.rs
@@ -1,38 +1,12 @@
 mod configuration;
+pub use configuration::Config;
 
+use crate::prelude::*;
 use std::{
+    any::{Any, TypeId},
+    borrow::Borrow,
+    collections::HashMap,
+    fmt::{Debug, Display},
     ops::Deref,
     sync::{Arc, Mutex},
 };
-
-pub struct AppData(Arc<Shared>);
-
-impl AppData {
-    pub fn new() -> Self {
-        Self(Arc::new(Shared::new()))
-    }
-}
-
-impl Clone for AppData {
-    fn clone(&self) -> Self {
-        Self(Arc::clone(&self.0))
-    }
-}
-
-impl Deref for AppData {
-    type Target = Arc<Shared>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-pub struct Shared {
-    pub counter: u32,
-}
-
-impl Shared {
-    pub fn new() -> Self {
-        Self { counter: 0 }
-    }
-}
