f452c81f3909f1ff7b7ff5f2880fbce0d7975026
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-11-20 15:49:35 +0900

    Refactor server and db configurations.

--- monoglu-platter/src/configuration.rs
+++ monoglu-platter/src/configuration.rs
@@ -1,16 +1,13 @@
 use crate::prelude::*;
 use serde::{Deserialize, Serialize};
-use std::{fs, path::PathBuf};
+use std::{fs, io::stdin, path::PathBuf};
 
 #[derive(Deserialize, Serialize)]
 pub struct Config {
     // server address
-    pub ip: String,
-    pub port: u16,
+    pub server: Server,
     // database address and auth
-    pub database: String,
-    pub db_id: String,
-    pub db_password: String,
+    pub database: Database,
 }
 
 impl Config {
@@ -30,11 +27,79 @@ impl Config {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            ip: "127.0.0.1".to_string(),
-            port: 50000,
-            database: "127.0.0.1:8086".to_string(),
-            db_id: "kanet".to_string(),
-            db_password: "01076737627".to_string(),
+            server: Server::default(),
+            database: Database::default(),
         }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Server {
+    ip: String,
+    port: u16,
+}
+
+impl Server {
+    pub fn address(&self) -> String {
+        format!("{}:{}", &self.ip, &self.port)
+    }
+}
+
+impl Default for Server {
+    fn default() -> Self {
+        Self {
+            ip: "127.0.0.1".to_string(),
+            port: 50000
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Database {
+    address: Option<String>,
+    username: Option<String>,
+    password: Option<String>,
+}
+
+impl Database {
+    fn init(mut self) -> Self {
+        if self.address == None {
+            error!("The database address is empty.");
+            info!("[address]");
+            self.address = Some(user_input());
+        }
+        if self.username == None || self.password == None {
+            info!("Does the db require user auth? Y/N");
+            match user_input().trim().to_lowercase().as_str() {
+                "y" => {
+                    Self::set_auth(&mut self);
+                    info!("Updated the auth info.");
+                },
+                "n" => {
+                    info!("Auth info set to 'None'.");
+                },
+                _ => {
+                    info!("Auth info set to 'None'.")
+                },
+            }
+        }
+        self
+    }
+
+    fn set_auth(&mut self) {
+        info!("[username]");
+        self.username = Some(user_input());
+        info!("[password]");
+        self.password = Some(user_input());
+    }
+}
+
+impl Default for Database {
+    fn default() -> Self {
+        Self {
+            address: None,
+            username: None,
+            password: None,
+        }.init()
     }
 }
