207f343964a32e80734e499f49a3781cf18d8bb0
Allen Clarence <zombiedelah@gmail.com>
2022-11-23 01:40:48 +0900

    Basic quic server working in progress..

--- monoglu-testserver/src/lib.rs
+++ monoglu-testserver/src/lib.rs
@@ -1,54 +1,40 @@
-mod data;
+mod config;
 
-use actix_files as fs;
-use actix_web::{
-    get, post, web, 
-    App, Error, HttpRequest, HttpServer, HttpResponse
-};
-use data::*;
-use futures::StreamExt;
-use std::path::PathBuf;
-use tracing::info;
+pub mod prelude {
+    pub type Exception = Box<dyn std::error::Error>;
+    pub type Result<T> = std::result::Result<T, Exception>;
 
-#[get("/")]
-async fn index() -> Result<fs::NamedFile, Error> {
-    let path = PathBuf::from("C:\\Projects\\monoglu\\monoglu-webapp\\dist\\index.html");
-    let file = fs::NamedFile::open(path)?;
-    Ok(file)
+    pub use crate::config::Configuration;
+    pub use tracing::{error, info};
 }
 
-#[get("/{file_name}")]
-async fn static_file(request: HttpRequest) -> Result<fs::NamedFile, Error> {
-    let mut path = PathBuf::from("C:\\Projects\\monoglu\\monoglu-webapp\\dist");
-    let file_name = request.match_info().query("file_name");
-    path.push(file_name);
-    let file = fs::NamedFile::open(path)?;
-    Ok(file)
-}
+pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-29"];
 
-#[post("/auth")]
-async fn auth(mut payload: web::Payload) -> Result<HttpResponse, Error> {
-    let mut body = web::BytesMut::new();
-    while let Some(chunk) = payload.next().await {
-        let chunk = chunk?;
-        body.extend_from_slice(&chunk);
+use crate::prelude::*;
+use std::sync::Arc;
+
+#[tokio::main]
+pub async fn run_server() -> Result<()> {
+    let config = Configuration::from_file("./Config.toml")?;
+    let (cert, key) = config.tls.load_cert()?;
+    
+    let mut server_crypto = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(vec![cert], key)?;
+    server_crypto.alpn_protocols = ALPN_QUIC_HTTP.iter().map(|&x| x.into()).collect();
+
+    let mut server_config = quinn::ServerConfig::with_crypto(Arc::new(server_crypto));
+    Arc::get_mut(&mut server_config.transport)
+        .unwrap()
+        .max_concurrent_uni_streams(config.server.max_concurrency.into());
+
+    let endpoint = quinn::Endpoint::server(server_config, config.server.endpoint_v4()?)?;
+    info!("Listening on {}", endpoint.local_addr()?);
+
+    while let Some(connection) = endpoint.accept().await {
+        info!("{:?}", connection);
     }
-    let user_info = serde_json::from_slice::<UserInfo>(&body)?;
-    info!("{:?}", user_info);
-    Ok(HttpResponse::Ok().json(user_info.id))
-}
 
-pub async fn run_app() -> Result<(), Box<dyn std::error::Error>> {
-    info!("Initializing the server at http://127.0.0.1:8080");
-    HttpServer::new(move || {
-        App::new()
-            .service(index)
-            .service(static_file)
-            .service(auth)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-    .unwrap();
     Ok(())
 }
