8e454015fbce88f2bca3e3485cf388d1a396b500
Allen Clarence <zombiedelah@gmail.com>
2022-11-27 16:02:01 +0900

    Remove modules for a new QUIC implementation test.

--- monoglu-testserver/src/data/user_info.rs
+++
@@ -1,7 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Debug, serde::Deserialize)]
-pub struct UserInfo {
-    pub id: String,
-    pub password: String,
-}
--- monoglu-testserver/src/main.rs
+++
@@ -1,9 +0,0 @@
-#[actix_web::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt()
-        .with_line_number(true)
-        .with_thread_ids(true)
-        .init();
-    monoglu_testserver::run_app().await?;
-    Ok(())
-}
