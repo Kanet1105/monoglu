6197243c16f2241bbd7d0f3070f29f3ecd006ac5
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-11-28 18:19:36 +0900

    QUIC implementation in progress..

---
+++ monoglu-testserver/src/crypto.rs
@@ -0,0 +1,16 @@
+use crate::prelude::*;
+use std::fs;
+use std::path::PathBuf;
+
+pub fn generate_pem(path: &str, name: &str) -> Result<()> {
+    fs::create_dir_all(path)?;
+    let mut cert_path = PathBuf::from(path);
+    cert_path.push(format!("{}_cert.pem", name));
+    let mut key_path = PathBuf::from(path);
+    key_path.push(format!("{}_key.pem", name));
+
+    let cert = rcgen::generate_simple_self_signed(vec![format!("{}", name)])?;
+    fs::write(&cert_path, cert.serialize_pem()?)?;
+    fs::write(&key_path, cert.serialize_private_key_pem())?;
+    Ok(())
+}
--- monoglu-testserver/src/lib.rs
+++ monoglu-testserver/src/lib.rs
@@ -1,40 +1,121 @@
-mod config;
+mod crypto;
 
 pub mod prelude {
-    pub type Exception = Box<dyn std::error::Error>;
-    pub type Result<T> = std::result::Result<T, Exception>;
+    pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-    pub use crate::config::Configuration;
     pub use tracing::{error, info};
 }
 
-pub const ALPN_QUIC_HTTP: &[&[u8]] = &[b"hq-29"];
-
 use crate::prelude::*;
-use std::sync::Arc;
+use ring::rand::{SecureRandom, SystemRandom};
+use std::{io::ErrorKind, net::SocketAddr, sync::Arc};
+use tokio::{net::UdpSocket, io::AsyncWriteExt};
 
+/// ### REQUIREMENTS
+/// * CMAKE
+/// * NASM
 #[tokio::main]
 pub async fn run_server() -> Result<()> {
-    let config = Configuration::from_file("./Config.toml")?;
-    let (cert, key) = config.tls.load_cert()?;
-    
-    let mut server_crypto = rustls::ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_single_cert(vec![cert], key)?;
-    server_crypto.alpn_protocols = ALPN_QUIC_HTTP.iter().map(|&x| x.into()).collect();
+    // crypto::generate_pem("./cert", "temp")?;
+    let mut socket = UdpSocket::bind("127.0.0.1:5000").await?;
 
-    let mut server_config = quinn::ServerConfig::with_crypto(Arc::new(server_crypto));
-    Arc::get_mut(&mut server_config.transport)
-        .unwrap()
-        .max_concurrent_uni_streams(config.server.max_concurrency.into());
+    // https://github.com/cloudflare/quiche/blob/master/quiche/examples/server.rs#L78
+    let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION)?;
+    config.load_cert_chain_from_pem_file("./cert/temp_cert.pem")?;
+    config.load_priv_key_from_pem_file("./cert/temp_key.pem")?;
+    config.set_application_protos(&[b"hq-interop", b"hq-29", b"hq-28", b"hq-27", b"http/0.9"])?;
 
-    let endpoint = quinn::Endpoint::server(server_config, config.server.endpoint_v4()?)?;
-    info!("Listening on {}", endpoint.local_addr()?);
+    let rng = ring::rand::SystemRandom::new();
+    let connection_id_seed =
+        ring::hmac::Key::generate(ring::hmac::HMAC_SHA256, &rng).unwrap();
 
-    while let Some(connection) = endpoint.accept().await {
-        info!("{:?}", connection);
+    let mut socket_buffer = Vec::<u8>::with_capacity(1024);
+    loop {
+        match socket.recv_from(&mut socket_buffer).await {
+            Ok((length, remote_address)) => {
+                let header = quiche::Header::from_slice(
+                    &mut socket_buffer[0..length],
+                    quiche::MAX_CONN_ID_LEN,
+                )?;
+                let tag = ring::hmac::sign(
+                    &connection_id_seed,
+                    &header.dcid,
+                );
+                let connection_id = &tag
+                    .as_ref()[..quiche::MAX_CONN_ID_LEN]
+                    .to_vec()
+                    .into();
+                let connection = quiche::accept(
+                    connection_id,
+                    None,
+                    socket.local_addr()?,
+                    remote_address.clone(),
+                    &mut config,
+                )?;
+                handle_connection(connection).await;
+            },
+            Err(e) => {
+                if e.kind() == ErrorKind::WouldBlock {
+                    continue;
+                } else {
+                    error!("{}", e);
+                    panic!();
+                }
+            }
+        };
     }
+}
 
+pub async fn handle_connection(connection: quiche::Connection) {
+    if connection.is_established() {
+        info!("Connection established..");
+    }
+}
+
+#[tokio::main]
+pub async fn run_client() -> Result<()> {
+    use tokio::time::{sleep, Duration};
+
+    let mut socket = UdpSocket::bind("127.0.0.1:0").await?;
+    let server_addr = "127.0.0.1:5000".parse::<SocketAddr>()?;
+
+    let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION)?;
+    config.verify_peer(false);
+    config.set_application_protos(&[b"hq-interop", b"hq-29", b"hq-28", b"hq-27", b"http/0.9"])?;
+
+    let mut scid = [0; quiche::MAX_CONN_ID_LEN];
+    SystemRandom::new().fill(&mut scid[..]).unwrap();
+    let scid = quiche::ConnectionId::from_ref(&scid);
+
+    let mut connection = quiche::connect(
+        Some("TestServer"), 
+        &scid, 
+        socket.local_addr()?,
+        server_addr,
+        &mut config,
+    )?;
+
+    let mut recv_buffer = Vec::<u8>::with_capacity(64);
+    tokio::spawn(async move {
+        loop {
+            
+        }
+    });
+
+    let mut send_buffer = Vec::<u8>::with_capacity(64);
+    loop {
+        for i in 0..100 {
+            send_buffer.write_u64(i as u64);
+            let (write, send_info) = match connection.send(&mut send_buffer) {
+                Ok(v) => v,
+                Err(e) => {
+                    error!("{}", e);
+                    break;
+                },
+            };
+            sleep(Duration::from_millis(500)).await;
+            send_buffer.clear();
+        }
+    }
     Ok(())
 }
