21c2358717e02b54b3e5437d771f1881f8c841d2
Allen Clarence <zombiedelah@gmail.com>
2022-11-29 01:22:12 +0900

    Adding stream in progress..

--- monoglu-testserver/src/lib.rs
+++ monoglu-testserver/src/lib.rs
@@ -7,9 +7,15 @@ pub mod prelude {
 }
 
 use crate::prelude::*;
+use bytes::BufMut;
 use ring::rand::{SecureRandom, SystemRandom};
-use std::{io::ErrorKind, net::SocketAddr, sync::Arc};
-use tokio::{net::UdpSocket, io::AsyncWriteExt};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::net::UdpSocket;
+use tokio::sync::Mutex;
+
+static MAX_DATAGRAM_SIZE: usize = 1350;
 
 /// ### REQUIREMENTS
 /// * CMAKE
@@ -24,51 +30,83 @@ pub async fn run_server() -> Result<()> {
     config.load_cert_chain_from_pem_file("./cert/temp_cert.pem")?;
     config.load_priv_key_from_pem_file("./cert/temp_key.pem")?;
     config.set_application_protos(&[b"hq-interop", b"hq-29", b"hq-28", b"hq-27", b"http/0.9"])?;
+    config.set_max_idle_timeout(5000);
+    config.set_max_recv_udp_payload_size(MAX_DATAGRAM_SIZE);
+    config.set_max_send_udp_payload_size(MAX_DATAGRAM_SIZE);
+    config.set_initial_max_data(10_000_000);
+    config.set_initial_max_stream_data_bidi_local(1_000_000);
+    config.set_initial_max_stream_data_bidi_remote(1_000_000);
+    config.set_initial_max_stream_data_uni(1_000_000);
+    config.set_initial_max_streams_bidi(100);
+    config.set_initial_max_streams_uni(100);
+    config.set_disable_active_migration(true);
+    config.enable_early_data();
 
     let rng = ring::rand::SystemRandom::new();
-    let connection_id_seed =
-        ring::hmac::Key::generate(ring::hmac::HMAC_SHA256, &rng).unwrap();
+    let connection_id_seed = ring::hmac::Key::generate(ring::hmac::HMAC_SHA256, &rng).unwrap();
 
-    let mut socket_buffer = Vec::<u8>::with_capacity(1024);
+    let mut pool = HashMap::<quiche::ConnectionId<'static>, Arc<Mutex<quiche::Connection>>>::new();
+
+    let mut recv_buffer = vec![0; MAX_DATAGRAM_SIZE];
+    // let mut output_buffer = vec![0; MAX_DATAGRAM_SIZE];
     loop {
-        match socket.recv_from(&mut socket_buffer).await {
-            Ok((length, remote_address)) => {
-                let header = quiche::Header::from_slice(
-                    &mut socket_buffer[0..length],
-                    quiche::MAX_CONN_ID_LEN,
-                )?;
-                let tag = ring::hmac::sign(
-                    &connection_id_seed,
-                    &header.dcid,
-                );
-                let connection_id = &tag
-                    .as_ref()[..quiche::MAX_CONN_ID_LEN]
-                    .to_vec()
-                    .into();
-                let connection = quiche::accept(
-                    connection_id,
-                    None,
-                    socket.local_addr()?,
-                    remote_address.clone(),
-                    &mut config,
-                )?;
-                handle_connection(connection).await;
-            },
-            Err(e) => {
-                if e.kind() == ErrorKind::WouldBlock {
-                    continue;
-                } else {
-                    error!("{}", e);
-                    panic!();
-                }
+        let (len, remote_addr) = socket.recv_from(&mut recv_buffer).await?;
+        let header = quiche::Header::from_slice(&mut recv_buffer[0..len], quiche::MAX_CONN_ID_LEN)?;
+        let connection_id: quiche::ConnectionId = ring::hmac::sign(&connection_id_seed, &header.dcid)
+            .as_ref()[..quiche::MAX_CONN_ID_LEN]
+            .to_vec()
+            .into();
+        let stream = if !pool.contains_key(&connection_id) {
+            // Check if the packet type is 'Initial'.
+            if header.ty != quiche::Type::Initial {
+                error!("Packet type is not 'Initial'.");
+                continue;
             }
+
+            // Version negotiation.
+            // if quiche::version_is_supported(header.version) {
+            //     info!("Negotiating the version.");
+            //     let n = quiche::negotiate_version(&header.scid, &header.dcid, &mut output_buffer)?;
+            //     if let Err(e) = socket.send_to(&output_buffer[0..n], &remote_addr).await {
+            //         if e.kind() == std::io::ErrorKind::WouldBlock {
+            //             error!("send_to() would block.");
+            //             break;
+            //         }
+            //         panic!();
+            //     }
+            //     continue;
+            // }
+
+            let connection = quiche::accept(
+                &connection_id,
+                None,
+                socket.local_addr()?,
+                remote_addr,
+                &mut config,
+            )?;
+            info!("Connected!");
+            let stream = Arc::new(Mutex::new(connection));
+            pool.insert(connection_id.clone(), stream.clone());
+            stream
+        } else {
+            let stream = Arc::clone(pool.get(&connection_id).unwrap());
+            stream
         };
+        tokio::spawn(handle_stream(stream));
     }
 }
 
-pub async fn handle_connection(connection: quiche::Connection) {
-    if connection.is_established() {
-        info!("Connection established..");
+pub async fn handle_stream(stream: Arc<Mutex<quiche::Connection>>) {
+    let mut recv_buffer = vec![0; MAX_DATAGRAM_SIZE];
+    loop {
+        let mut stream_guard = stream.lock().await;
+        let (len, is_fin) = match stream_guard.stream_recv(0, &mut recv_buffer) {
+            Ok(v) => v,
+            Err(_) => {
+                continue;
+            },
+        };
+        stream_guard.stream_send(0, &recv_buffer[0..len], is_fin).unwrap();
     }
 }
 
@@ -82,40 +120,60 @@ pub async fn run_client() -> Result<()> {
     let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION)?;
     config.verify_peer(false);
     config.set_application_protos(&[b"hq-interop", b"hq-29", b"hq-28", b"hq-27", b"http/0.9"])?;
+    config.set_max_idle_timeout(5000);
+    config.set_max_recv_udp_payload_size(MAX_DATAGRAM_SIZE);
+    config.set_max_send_udp_payload_size(MAX_DATAGRAM_SIZE);
+    config.set_initial_max_data(10_000_000);
+    config.set_initial_max_stream_data_bidi_local(1_000_000);
+    config.set_initial_max_stream_data_bidi_remote(1_000_000);
+    config.set_initial_max_streams_bidi(100);
+    config.set_initial_max_streams_uni(100);
+    config.set_disable_active_migration(true);
 
     let mut scid = [0; quiche::MAX_CONN_ID_LEN];
-    SystemRandom::new().fill(&mut scid[..]).unwrap();
+    // SystemRandom::new().fill(&mut scid[..]).unwrap();
     let scid = quiche::ConnectionId::from_ref(&scid);
 
     let mut connection = quiche::connect(
-        Some("TestServer"), 
-        &scid, 
+        Some("TestServer"),
+        &scid,
         socket.local_addr()?,
         server_addr,
         &mut config,
     )?;
 
-    let mut recv_buffer = Vec::<u8>::with_capacity(64);
-    tokio::spawn(async move {
-        loop {
-            
-        }
-    });
+    info!(
+        "connecting to {:} from {:} with scid {:x?}",
+        &server_addr,
+        socket.local_addr()?,
+        &scid,
+    );
 
-    let mut send_buffer = Vec::<u8>::with_capacity(64);
-    loop {
-        for i in 0..100 {
-            send_buffer.write_u64(i as u64);
-            let (write, send_info) = match connection.send(&mut send_buffer) {
-                Ok(v) => v,
-                Err(e) => {
-                    error!("{}", e);
-                    break;
-                },
-            };
-            sleep(Duration::from_millis(500)).await;
-            send_buffer.clear();
-        }
+    let mut sock_buffer = vec![0; MAX_DATAGRAM_SIZE];
+    let (len, send_info) = connection.send(&mut sock_buffer)?;
+    socket.send_to(&sock_buffer[..len], &send_info.to).await?;
+
+    let sender = Arc::new(Mutex::new(connection));
+    let receiver = sender.clone();
+    let stream_id = 0;
+
+    // if connection.is_established() {
+    //     // let mut recv_stream = vec![0; MAX_DATAGRAM_SIZE];
+    //     // tokio::spawn(async move {
+    //     //     loop {
+    //     //         let mut receiver = receiver.lock().await;
+    //     //         let (len, is_fin) = receiver.stream_recv(stream_id, &mut recv_stream).unwrap();
+    //     //         info!("{}, {}", len, is_fin);
+    //     //     }
+            
+    //     // });
+    // }
+    let mut send_stream = vec![0; MAX_DATAGRAM_SIZE];
+    for packet in 0..100 {
+        let mut sender = sender.lock().await;
+        send_stream.put_u64(packet as u64);
+        sender.stream_send(stream_id, &send_stream, true)?;
+        sleep(Duration::from_millis(500)).await;
     }
     Ok(())
 }
