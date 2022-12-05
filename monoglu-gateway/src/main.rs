f7df55d9cdef8297d88c42dd57921ba7de8b62da
Donghoon Lee <90014039+Kanet1105@users.noreply.github.com>
2022-12-05 13:57:58 +0900

    Test gateway..

---
+++ monoglu-gateway/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind(("192.168.1.2", 50000))?
+    .run()
+    .await
+}
